ac335424e24af13c72867050f5fc708275935b86
azizcan.varol <azizcan.varol@teknosa.com>
2024-05-03 20:17:09 +0300

    run_tests.py Updated

--- run_tests.py
+++ run_tests.py
@@ -1,36 +1,15 @@
 import subprocess
-import time
 import sys
-
-def check_grid_health():
-    """
-    Selenium Grid sağlığını kontrol etmek için bir işlev.
-    Selenium Grid sağlıklı bir şekilde çalışıyorsa True döndürür, aksi takdirde False döndürür.
-    """
-    try:
-        subprocess.run(["docker", "ps"], check=True)
-        # Selenium Grid konteynerleri çalışıyor, sağlıklı
-        return True
-    except subprocess.CalledProcessError:
-        # Selenium Grid konteynerleri çalışmıyor, sağlıksız
-        return False
 
 def run_tests(node_count):
     """
     Belirtilen düğüm sayısına göre Docker konteynerlerini başlatır ve testleri çalıştırır.
     """
-    # Selenium Grid sağlığını kontrol et
-    if not check_grid_health():
-        print("Selenium Grid is not healthy. Aborting test execution.")
-        return
-    
     # Docker konteynerlerini başlat
     subprocess.run(["docker-compose", "up", "-d", "--scale", f"selenium-node-chrome={node_count}"])
     
-    subprocess.run(["docker", "ps", "-a"])
-
-    # Selenium konteynerini başlatıp testleri çalıştır
-    subprocess.run(["docker-compose", "exec", "jenkinstask-selenium-tests", "python", "-m", "pytest", "--html=report.html"])
+    # Testleri çalıştır
+    subprocess.run(["docker-compose", "exec", "selenium-tests", "python", "-m", "pytest", "--html=report.html"])
 
     # Docker konteynerlerini kapat
     subprocess.run(["docker-compose", "down"])
